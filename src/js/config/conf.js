8c5e5d3ff81f46eabfa5c9e4967e986c94eaf841
cirm <m.jullinen@gmail.com>
2015-10-15 15:54:48 +0300

    Little cleanup - remove old files.

--- src/js/config/conf.js
+++ src/js/config/conf.js
@@ -12,8 +12,6 @@
       envString   : 'development',
       port        : 8080,
       logFormat   : 'dev',
-      conString   : 'mongodb://localhost:27017/lindoria',
-      pgConnString: 'postgres://geegomoonshine:ToomasOnHeaDm123@localhost:5432/lindoriadb',
       tokenSecret : fs.readFileSync(rootPath + 'ssl/pub_lind_token.pem'),
       tokenOptions: {
         algorithm: 'HS256',
@@ -32,8 +30,6 @@
       port        : 8080,
       envString   : 'staging',
       logFormat   : 'dev',
-      conString   : 'mongodb://db:27017/lindoria',
-      pgConnString: 'postgres://geegomoonshine:ToomasOnHeaDm123@db:5432/lindoriadb',
       tokenSecret : fs.readFileSync(rootPath + 'ssl/pub_lind_token.pem'),
       tokenOptions: {
         algorithm: 'HS256',
