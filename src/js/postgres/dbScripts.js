1cc019e59163c9d2a9be5a35cd7557d0a3c5db32
cirm <m.jullinen@gmail.com>
2015-12-03 22:45:34 +0200

    At last, finished somewhat stable provisioning buildup.

--- src/js/postgres/dbScripts.js
+++ src/js/postgres/dbScripts.js
@@ -8,37 +8,56 @@
   var conf           = require('../config/conf');
   var releaseVersion = conf.db.dbVersion;
 
-  var updateDb = function (content) {
-    return clientPool.query(content, []);
+
+  var updateDb = function (scriptArr) {
+    return clientPool.query(scriptArr[0], [])
+      .then(function () {
+        return dbFunction('dbv.log_update', scriptArr[1], scriptArr[2]);
+      });
   };
 
   var dbFunction = function (fname, scriptVersion, comment) {
     return clientPool.queryFunction(fname, [scriptVersion, comment]);
   };
 
+  var readDir = function () {
+    return fs.readdirAsync('./postgres/provision/')
+  };
+
+  var readFiles = function (fileArr, dbVersion) {
+    return Promise.map(fileArr, function (file) {
+      var scriptVersion = parseInt(file.match(/\d+\./));
+      if (scriptVersion > dbVersion && scriptVersion <= releaseVersion) {
+        var comment = /.{3}\.(\w+)\.sql/.exec(file)[1];
+        return fs.readFileAsync('./postgres/provision/' + file, 'utf8')
+          .then(function (script) {
+              return [script, scriptVersion, comment];
+            }
+          );
+      } else {
+        return false;
+      }
+    })
+  };
 
   var getFilesForUpdate = function (dbVersion) {
-    return fs.readdirAsync('./postgres/provision/')
-      .map(function (file) {
-        console.log(file);
-        var scriptVersion = parseInt(file.match(/\d+\./));
-        if (scriptVersion > dbVersion && scriptVersion <= releaseVersion) {
-          var comment = /.{3}\.(\w+)\.sql/.exec(file)[1];
-          return fs.readFileAsync('./postgres/provision/' + file, 'utf8')
-            .then(function (content) {
-              return updateDb(content);
-            })
-            .then(function () {
-              return dbFunction('dbv.log_update', scriptVersion, comment);
-            })
-            .catch(function (err) {
-              console.log(err.toString());
-              return clientPool.query('ROLLBACK;', []);
-
-            });
-        }
-      });
+    return readDir()
+      .then(function (fileArr) {
+        return readFiles(fileArr, dbVersion)
+      })
+      .then(function (scriptArr) {
+        return Promise.each(scriptArr, function (sqlScript) {
+          if (sqlScript !== false) {
+            return updateDb(sqlScript)
+          }
+        });
+      })
+      .catch(function (err) {
+        console.log(err.toString());
+        return clientPool.query('ROLLBACK;', [])
+      })
   };
+
 
   module.exports = {
     runDbProvision: getFilesForUpdate
